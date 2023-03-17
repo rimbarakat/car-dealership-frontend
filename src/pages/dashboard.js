2613ad571947aeef19032e682e045b8952f3439c
Ahmad El Harake <aeharake@gmail.com>
2023-03-17 19:46:32 +0100

    use baseUrl from environment
    you should make sure to store all the env variables here

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../css/dashboard.css";
 import ProductItem from "../components/product-item/product-item";
 import { useQuery } from "react-query";
