70c8af8d2b1b288e7fe3dd788bc739edcb968c19
Khaled Hatoum <khaledhatoum@Khaleds-MacBook.lan>
2023-03-25 17:44:49 +0200

    fixed dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,7 +4,7 @@ import ProductItem from "../components/product-item/product-item";
 import { useQuery } from "react-query";
 import { getCars } from "../api/cars.service";
 function Dashboard() {
-  const { data: products, error, isLoading } = useQuery("getCars", getCars);
+  const { data, error, isLoading } = useQuery("getCars", getCars);
   
   if (error) {
     return <div>Error!</div>;
@@ -16,7 +16,7 @@ function Dashboard() {
     <div className="dashboard-container">
       <h1 className="dashboard-title">Product List</h1>
       <div className="dashboard-list">
-        {products.map((product) => (
+        {data.data.map((product) => (
           <ProductItem
             id={product.id}
             image={product.image}
