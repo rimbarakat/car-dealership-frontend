8cebc4da35774dc037cd356863a8533ffb870f63
Bassel Isber <basex@Bassels-MacBook-Pro.local>
2023-03-29 21:39:48 +0200

    added a functional search bar

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,9 +4,12 @@ import ProductItem from "../components/product-item/product-item";
 import { useQuery } from "react-query";
 import { getCars } from "../api/cars.service";
 import {Link} from "react-router-dom";
+import { useState } from "react";
+
 
 function Dashboard() {
   const { data, error, isLoading } = useQuery("getCars", getCars);
+  const [searchTerm,setSearchTerm]=useState('');
   const handleEdit = (id) => {
     // handle edit action
     console.log(`Edit product with id: ${id}`);
@@ -24,13 +27,25 @@ function Dashboard() {
   if (isLoading) {
     return <div>Loading...</div>;
   }
+  const filteredData = data.data.filter((product) =>
+    product.model.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">Product List</h1>
+      <div className="dashboard-search">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}/>
+      </div>
       <div className="dashboard-list">
-        {data.data.map((product) => (
+        {filteredData.map((product) => (
 
           <ProductItem
+            key={product._id}
             id={product._id}
             image={product.image}
             title={product.model}
