a49d24eb234ed480ad75c5c764c5220e872319e0
Mohammad Chahine <mohito.c2002@gmail.com>
2023-03-31 12:56:23 +0300

    fixed cardetails

--- src/pages/cardetails.js
+++ src/pages/cardetails.js
@@ -21,25 +21,39 @@ function CarDetails() {
     </div>
 
 
-    <div className='Title'>
+    <div className="pic-div"> <img className="Pic" src={data.image}></img> </div>
+
+    {/* <div className='Title'>
       <h> {data.model} {data.year}</h>
-    </div>
+    </div> */}
         
-    <div> <img className="Pic" src={data.image}></img> </div>
       
     <div className="PriceBox">
-      <h className="PriceH">$259,000</h>
-      <p className="PriceP">From $2,463.71 /month</p>
+      <h className="PriceH">{data.model}&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;{data.price}</h>
       </div>
-      {/* <div></div>
-      <div></div> */}
-      
-      
-    
 
+    
     <div className="Line1"></div>
-    <div className="Infoo"> <p> Year &emsp;&emsp;&emsp; Mileage &emsp;&emsp;&emsp; Engine &emsp;&emsp;&emsp; Gearbox &emsp;&emsp;&emsp; Drive &emsp;&emsp;&emsp; Fuel type</p></div>
-    <div className="Infoo"> <p> {data.year} &emsp;&emsp;&ensp; 3,000 km &emsp;&emsp;&ensp; 6 Cylinder &emsp;&ensp;  Manual &emsp;&emsp;&emsp;&ensp;&nbsp;Lhd &emsp;&emsp;&emsp;&ensp;&nbsp;&nbsp;Petrol</p></div>
+  <div className="table-div">
+    <table className="infotable">
+  <tr>
+    <td className="cell">Year</td>
+    <td className="cell">Mileage</td>
+    <td className="cell">Engine</td>
+    <td className="cell">Gearbox</td>
+    <td className="cell">Drive</td>
+    <td className="cell">Fuel type</td>
+  </tr>
+  <tr>
+    <td className="cell">{data.year}</td>
+    <td className="cell">{data.mileage} km</td>
+    <td className="cell">{data.engineShort}</td>
+    <td className="cell">{data.engineShort}</td>
+    <td className="cell">{data.gearBoxShort}</td>
+    <td className="cell">{data.driveShort} </td>
+  </tr>
+</table>
+</div>
     <div className="Line2"></div>
 
     <div>
@@ -48,14 +62,14 @@ function CarDetails() {
       <div className="Line3"></div>
       <p className="detailz">Car Details</p>
         <div className="detailzinfo">
-        <p>Model:&emsp;&emsp;&emsp;&emsp;&emsp;<strong>{data.model}</strong></p>
-        <p>Year:&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&#8196;<strong>{data.year}</strong></p>  
-        <p>Color:&emsp;&emsp;&emsp;&emsp;&#8199;&#8194;&#8200;<strong>{data.color}</strong></p>  
-        <p>Mileage:{data.mileage}</p>  
-        <p>Engine:{data.engine}</p>  
-        <p>Fuel Type:{data.fueltype}</p>  
-        <p>Gear Box:{data.gearbox}</p>  
-        <p>Drive:{data.drive}</p>  
+        <p><strong>Model:</strong>&emsp;&emsp;&emsp;&emsp;&emsp;{data.model}</p>
+        <p><strong>Year:</strong>&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&#8196;&#8202;{data.year}</p>  
+        <p><strong>Color:</strong>&emsp;&emsp;&emsp;&emsp;&#8199;&#8194;&#8200;{data.color}</p>  
+        <p><strong>Mileage:</strong>&emsp;&emsp;&#8197;&#8197;&#8201;&#8239;&#8202;&#8202;&#8202;&#8194;&#8194;&#8202;&#8202;&#8202;&#8202;&#8202;&#8202;&#8202;&#8202;&#8202;{data.mileage} km</p>   
+        <p><strong>Engine:</strong>&emsp;&emsp;&emsp;&emsp;&#8199;&#8201;{data.engine}</p>   
+        <p><strong>Fuel Type:</strong>&emsp;&emsp;&emsp;&#8239;&#8239;&#8202;&#8202;{data.fuelType}</p>  
+        <p><strong>Gear Box:</strong>&emsp;&emsp;&emsp;&#8239;&#8239;&#8239;&#8239;&#8239;&#8202;{data.gearBox}</p>   
+        <p><strong>Drive:</strong>&emsp;&emsp;&emsp;&emsp;&#8194;&#8194;&#8198;&#8202;&#8202;&#8239;&#8239;&#8202;{data.drive}</p>   
         </div>
   </div>
    
