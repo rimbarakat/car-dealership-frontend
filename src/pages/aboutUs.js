9e268738ed1965279c978735ed17efbc84b5babb
hekmat <hak68@mail.aub.edu>
2023-04-13 04:02:44 +0300

    Updated about us

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -5,7 +5,7 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       <h1 className="about-us-heading">About Us</h1>
-      <p className="about-us-description">We are a company that does XYZ.</p>
+      <p className="about-us-description">We are a car dealership based in Beirut, Lebanon. We offer premium cars for our customers to buy as well as the ability to test drive them. </p>
     </div>
   );
 }
