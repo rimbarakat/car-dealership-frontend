fcc6e505cd1b0fc7286abe402b17116cdfa75c73
Khaled Hatoum <khaledhatoum@Khaleds-MacBook.local>
2023-04-18 22:19:23 +0300

    modified car comparison from drop down menu

---
+++ src/components/comparison-item/comparison-item.jsx
@@ -0,0 +1,61 @@
+import "../product-item/product-item.css";
+import { isAdmin } from "../../utils";
+
+
+function ComparisonItem(props) {
+  const handleEdit = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    props.onEdit(props.id);
+  };
+
+  const handleDelete = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    props.onDelete(props.id);
+  };
+  
+
+  return (
+    
+      <div className="dashboard-item">
+        <p>
+          <img src={props.image} alt="#" />
+        </p>
+        
+        <h3 className="dashboard-item-title">{props.title}</h3>
+        <p className="dashboard-item-description">{/*props.description*/}</p>
+        <p className="dashboard-item-price">Price: ${props.price}</p>
+        
+        <div className="dashboard-item-buttons">
+          {isAdmin() ? (
+            <>
+              <button
+                className="dashboard-item-edit"
+                onClick={(event) => handleEdit(event)}
+              >
+                Edit
+              </button>
+              <button
+                className="dashboard-item-delete"
+                onClick={(event) => handleDelete(event)}
+              >
+                Delete
+              </button>
+            </>
+          ): null }
+        </div>
+        {props.isSold ? (
+          <div className="overlay">
+      <div className="sold-text">
+        Sold!
+      </div>
+    </div>
+        ): null}
+
+      </div>
+
+  );
+}
+
+export default ComparisonItem;
--- src/pages/CarComparison.js
+++ src/pages/CarComparison.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { getCars } from "../api/cars.service";
 import { getCar } from "../api/car.details";
-
+import { useMutation } from "react-query";
+import ComparisonItem from "../components/comparison-item/comparison-item";
 function CompareCars() {
   const [car1, setCar1] = useState("");
   const [car2, setCar2] = useState("");
+  const [car1Data, setCar1Data] = useState(null);
+  const [car2Data, setCar2Data] = useState(null);
 
   const { data, isLoading, error } = useQuery("getCars", getCars);
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -16,10 +18,17 @@ function CompareCars() {
   if (error) {
     return <div>Error!</div>;
   }
+  
+  const handleCompareClick=()=>{
+    const car1Data_ = data.data.find(car => car._id === car1);
+    const car2Data_ = data.data.find(car => car._id === car2); 
+    setCar1Data(car1Data_);
+    setCar2Data(car2Data_);
+  }
 
   const carOptions = data.data.map((car) => (
-    <option value={car.id} key={car.id}>
-      {car.model} ({car.year})
+    <option value={car._id} key={car._id}>
+      {car.model} ({car.year}) 
     </option>
   ));
 
@@ -48,7 +57,35 @@ function CompareCars() {
           {carOptions}
         </select>
       </div>
-      <button disabled={!car1 || !car2}>Compare</button>
+      <button disabled={!car1 || !car2} onClick={handleCompareClick}>Compare</button>
+      {car1Data && car2Data && (
+        <div>
+          <h2>{car1Data.model} ({car1Data.year}) vs {car2Data.model} ({car2Data.year})</h2>
+          <ComparisonItem
+            key={car1Data._id}
+            id={car1Data._id}
+            image={car1Data.image}
+            title={car1Data.model}
+            description={car1Data.description}
+            price={car1Data.price}
+            isSold={car1Data.isSold}
+            isAvailable={car1Data.isAvailable}
+            year={car1Data.year}
+          />
+          <ComparisonItem
+            key={car2Data._id}
+            id={car2Data._id}
+            image={car2Data.image}
+            title={car2Data.model}
+            description={car2Data.description}
+            price={car2Data.price}
+            isSold={car2Data.isSold}
+            isAvailable={car2Data.isAvailable}
+            year={car2Data.year}
+          />
+          
+        </div>
+      )}
     </div>
   );
 }
